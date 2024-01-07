cde27ac9941df0fddf3cdbf7e370a7a1b7b31e63
BIPODGHOSH <bipodghosh0@gmail.com>
2024-01-07 09:42:21 +0530

    adding navbar style and basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import Main from "./components/Main";
 import Signin from "./components/Signin";
 
 function App() {
   return (
-    <>
+    <div className="flex bg-slate-950 h-screen w-full">
       <Signin />
-    </>
+      <Main />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import logo from "../assets/images/logo.png";
+import user from "../assets/images/user.png";
+import home from "../assets/images/home.png";
+import search from "../assets/images/search.png";
+import love from "../assets/images/love.png";
+import action from "../assets/images/action.png";
+import comedy from "../assets/images/comedy.png";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [hover, setHover] = useState(false);
+  return (
+    <div className="bg-black grid grid-cols-2 h-screen w-28">
+      <div
+        className="left-side"
+        onMouseEnter={() => {
+          setHover(true);
+        }}
+        onMouseLeave={() => {
+          setHover(false);
+        }}
+      >
+        <img src={logo} alt="logo" className=" ml-5 mt-5" />
+        <img src={user} alt="user" className=" w-5 ml-9 mt-8 cursor-pointer" />
+        <img
+          src={search}
+          alt="search"
+          className=" w-9 ml-7 mt-8 cursor-pointer"
+        />
+        <img src={home} alt="home" className=" w-9 ml-7 mt-8 cursor-pointer" />
+
+        <img src={love} alt="love" className=" w-16 ml-5 mt-8 cursor-pointer" />
+        <img
+          src={comedy}
+          alt="comedy"
+          className=" w-16 ml-5 mt-8 cursor-pointer"
+        />
+        <img
+          src={action}
+          alt="action"
+          className=" w-20 ml-5 mt-8 cursor-pointer"
+        />
+      </div>
+      {hover && (
+        <div className="right-side ml-8 z-20 w-20 bg-black text-slate-300 text-base font-bold">
+          <h4 className="mt-20">Login</h4>
+          <h4 className="mt-9">Search</h4>
+          <h4 className="mt-12">Home</h4>
+          <h4 className="mt-10">Love</h4>
+          <h4 className="mt-9">Comedy</h4>
+          <h4 className="mt-10">Action</h4>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
